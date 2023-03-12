af07410d8c1b259743edd4b98efde8a2c519dba5
Yingxin Song <xinxin5@student.ubc.ca>
2023-03-12 04:38:46 -0700

    First row selection working!

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ teams = teams_df.Team.unique().tolist()
 
 drivers_df = pd.read_csv("data/formula1_2021season_drivers.csv")
 
+results = pd.read_csv('data/formula1_2021season_raceResults.csv')
+qual_results = pd.read_csv('data/formula1_2021season_sprintQualifyingResults.csv')
+
+t_pts = results.groupby('Team')['Points'].sum()
+st_pts = qual_results.groupby('Team')['Points'].sum()
+t_pts = (t_pts+st_pts).sort_values(ascending=False).reset_index()
+
+d_pts = results.groupby(['Driver', 'Team'])['Points'].sum()
+sd_pts = qual_results.groupby(['Driver', 'Team'])['Points'].sum()
+d_pts = (d_pts+sd_pts).sort_values(ascending=False).reset_index()
 
 
 app = dash.Dash(__name__,
@@ -24,28 +34,24 @@ app.layout = html.Div([
                 dbc.Row([
                     dbc.Col([
                         html.Div([
-                            'Choose a team:',
-                            dcc.Dropdown(
-                                id="team_selection",
-                                options = [{"label": t, "value": t} for t in teams],
-                                value =  teams_df.Team.values,
-                                placeholder = 'Select a team'),
+                            'Select a row to learn more about a team:',
                             dt.DataTable(
-                                id='teams_table',
-                                columns=[{"name": i, "id": i} for i in teams_df.columns],
-                                data=teams_df.to_dict("records"),
-                                fixed_columns={'headers': True, 'data': 1},
-                                style_table={'minWidth': '100%'},
+                                id='teams_all',
+                                columns=[{"name": i, "id": i} for i in t_pts.columns],
+                                data=t_pts.to_dict("records"),
+                                # fixed_columns={'headers': True, 'data': 1},
+                                # style_table={'minWidth': '100%'},
                                 style_data={
                                 'whiteSpace': 'normal',
-                                'height': 'auto'}
+                                'height': 'auto'},
+                                row_selectable = 'single'
                                 ),
                             ])]),
                     dbc.Col([
                         html.Br(),
                         html.Br(),
                         dt.DataTable(
-                            id='teams_table2',
+                            id='teams_table',
                             columns=[{"name": i, "id": i} for i in teams_df.columns],
                             data=teams_df.to_dict("records"),
                             fixed_columns={'headers': True, 'data': 1},
@@ -88,11 +94,16 @@ app.layout = html.Div([
 
 @app.callback(
     Output("teams_table", "data"),
-    Input("team_selection", "value")
+    Input("teams_all", "selected_rows")
 )
-def teams_display(team):
-    df = teams_df.query("Team == @team")
-    return df.to_dict("records")
+def teams_display(selected_rows):
+    print(selected_rows)
+    if selected_rows is None:
+        return teams_df.to_dict("records")
+    else:
+        team_name = t_pts.iloc[selected_rows[0],0]
+        df = teams_df.query("Team == @team_name")
+        return df.to_dict("records")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
